893c8a14978c8e69a5ca3d421c4bf9eff1cfeaa0
kjm3110 <kjm3110@naver.com>
2016-07-12 20:19:48 +0900

    My initial state

---
+++ app/src/main/java/com/jungmok/perfect_blue/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jungmok.perfect_blue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int a = 18;
+        
+    }
+}
